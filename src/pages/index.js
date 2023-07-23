27a424436589b4738533ee2be4256a0b7b78c2f7
RTemiy <RTemiyGo@yandex.ru>
2023-07-23 22:24:46 +0300

    reworked from layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,6 @@
 import './index.css'
+import reveal from "../components/Reveal";
 
-function reveal() {
-  const reveals = document.querySelectorAll(".reveal");
-  for (let i = 0; i < reveals.length; i++) {
-    let windowHeight = window.innerHeight;
-    let elementTop = reveals[i].getBoundingClientRect().top;
-    let elementVisible = 150;
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-}
 
 window.addEventListener("scroll", reveal);
 reveal();
