29a4703f836c8e75af54cce073513a95b2f33095
RTemiy <RTemiyGo@yandex.ru>
2023-07-25 18:50:58 +0300

    completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,16 @@
 import './index.css'
 import reveal from "../components/Reveal";
+import Gallery from "../components/Gallery";
 
 
 window.addEventListener("scroll", reveal);
 reveal();
+
+new Gallery("#gallery-about");
+new Gallery("#gallery-weekend");
+new Gallery("#gallery-myata");
+new Gallery("#gallery-full");
+new Gallery("#gallery-sunrise");
+new Gallery("#gallery-flowers");
+new Gallery("#gallery-seminars");
+new Gallery("#gallery-location");
